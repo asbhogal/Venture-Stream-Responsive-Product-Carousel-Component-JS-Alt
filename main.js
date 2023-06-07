2534c3c6f0f764f0cc8a3c66bf6b0f6b09764519
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-07 14:48:32 +0100

    added carousel and product cards

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
 import "./style.css";
+
+import products from "./src/js/constants/productsData";
+
+const createElement = (tag, attributes = {}, content = "") => {
+  const element = document.createElement(tag);
+  Object.entries(attributes).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+  element.innerHTML = content;
+  return element;
+};
+
+const carousel = document.querySelector(".carousel");
+
+products.forEach((product) => {
+  const productCard = createElement("div", {
+    key: product.id,
+    class: "product-card",
+  });
+  const productImg = createElement("img", {
+    class: "product-image",
+    src: product.imgSrc,
+    alt: product.imgAlt,
+  });
+  const productTitle = createElement(
+    "h3",
+    {
+      class: "product-title",
+    },
+    product.name
+  );
+  const arrowIcon = createElement("img", {
+    class: "arrow-icon",
+    src: "./icons/Arrow.svg",
+    alt: "Arrow icon",
+  });
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productTitle);
+  productCard.appendChild(arrowIcon);
+
+  carousel.appendChild(productCard);
+});
